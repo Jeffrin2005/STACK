77b07f51c344663b6f97e48d9d6f1e5a453409fd
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-25 13:23:16 +0530

    (STACK)Backspace String Compare.cpp

---
+++ (STACK)Backspace String Compare.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    private:
+// time comp = O(max(n,m)) space comp = o(max(n,m))
+    string processString( string& str){
+        stack<char>editor;
+        for(auto&c : str){
+            if(c != '#'){
+                editor.push(c);
+            }else if (!editor.empty()){// c=='#' so we are removing the letter bcoz backspace
+                editor.pop();
+            }
+        }
+        string result;
+        while(!editor.empty()){
+            result += editor.top();
+            editor.pop();
+        }
+        reverse(result.begin(), result.end()); // Reverse the result string
+        return result;
+    }
+public:
+    bool backspaceCompare(string s, string t) {
+        return processString(s) == processString(t);
+    }
+};
